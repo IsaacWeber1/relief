6d89d8f7e4199389862b520ce6c52d6da15c92f1
BrishnaN <brishna@udel.edu>
2025-03-01 15:31:38 -0500

    naviagtion button added

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -1,7 +1,12 @@
 // src/components/Home.tsx
 import React from "react";
+import GetHelp from "./GetHelp";
+import { useNavigate } from "react-router-dom";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import HelpOthers from "./HelpOthers";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Welcome to the Simulated Blood Drawing Routine Training Program</h1>
@@ -9,6 +14,14 @@ const Home = () => {
         This program helps train nurses on blood drawing routines. Use the
         navigation above to sign up or log in to get started.
       </p>
+      <Button
+        onClick={() => {
+          navigate("/GetHelp");
+        }}
+      >
+        Gethelp
+      </Button>
+      <Button onClick={() => navigate("/HelpOthers")}>HelpOthers</Button>
     </div>
   );
 };
